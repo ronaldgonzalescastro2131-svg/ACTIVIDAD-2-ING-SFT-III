ac3e79e37b7b2030811c306109ed529edb9adc28
Ronal <ronaldgonzalescastro2131@gmail.com>
2026-04-09 21:09:52 -0500

    Add GitHub Pages deployment workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+
+const repoName = 'ACTIVIDAD-2-ING-SFT-III';
+const base = process.env.NODE_ENV === 'production' ? `/${repoName}` : '';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base
+		}
 	}
 };
 
